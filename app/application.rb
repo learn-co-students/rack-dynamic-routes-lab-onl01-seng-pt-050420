e8ad8535e2e1e27514655d58408e098b645ad443
ryancdavey1 <ryancdavey1@gmail.com>
2020-07-29 22:33:53 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+require_relative("./item")
+
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      search_term = req.params["item"]
+      item = @@items.find{|i| i.name = search_term}
+      if item
+        resp.write "#{item.price}"
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.write = "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
